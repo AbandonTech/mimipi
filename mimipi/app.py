786afcffe1d901f7ca55850aedd57c68e51ecb73
GDWR <gregory.dwr@gmail.com>
2024-10-20 23:01:13 +0100

    Use scalar-fastapi for documentation

--- mimipi/app.py
+++ mimipi/app.py
@@ -3,14 +3,19 @@ import random
 from typing import Annotated, Literal
 
 from faker import Faker
-from fastapi import FastAPI, Header, Response
+from fastapi import FastAPI, Header, Response, Query
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
+from scalar_fastapi import get_scalar_api_reference
 
 from .product import Product
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Product API",
+    docs_url=None,
+    redoc_url=None,
+)
 
 
 def stream_csv(products):
@@ -19,14 +24,24 @@ def stream_csv(products):
         yield f"{product.upc},{product.name},{product.details},{product.weight}".replace("\n", "\\n") + "\n"
 
 
+@app.get("/docs", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title,
+        hide_download_button=True,
+        hide_models=False,
+    )
+
 @app.get("/product")
 def get_product(
     response: Response,
-    accept: Annotated[str|None, Header()] = None,
-    x_seed: Annotated[int|None, Header()] = None,
-    quantity: int = 1,
-    entropy: float = 0,
-):
+    accept: Annotated[str, Header()] = "text/csv",
+    x_seed: Annotated[int|None, Header(description="The seed used to generate the product list. When no X-Seed header has been provided, a random seed will be generated.")] = None,
+    quantity: int = Query(1, ge=1, description="Number of products to generate."),
+    entropy: float = Query(0.0, ge=0.0, le=1.0, description="The percent chance of switching formatting of each row before generating each row.")
+) -> Response:
+    """Get a list of products."""
     seed = x_seed or random.randint(0, 100000)
     response.headers["X-Seed"] = str(seed)
 
@@ -66,7 +81,7 @@ def get_product(
     rng.shuffle(products)
 
     match accept:
-        case "text/csv":
-            return StreamingResponse(content=stream_csv(products), media_type="text/csv", headers=response.headers)
-        case _:
+        case "application/json":
             return products
+        case _:
+            return StreamingResponse(content=stream_csv(products), media_type="text/csv", headers=response.headers)
