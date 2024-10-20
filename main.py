5f5c12d7ecb37883a7b2fdfe41171ed2f20fabe4
GDWR <gregory.dwr@gmail.com>
2024-10-20 19:28:35 +0100

    Add a seeded random product generator with CSV output support

--- main.py
+++ main.py
@@ -1,9 +1,58 @@
+import random
+
+from typing import Annotated
+
 from faker import Faker
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Response
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
 
 app = FastAPI()
-fake = Faker()
 
-@app.get("/")
-def get_root():
-    return fake.name()
+class Product(BaseModel):
+    upc: str
+    name: str
+    details: str
+    weight: int
+    barcode: str
+
+
+def stream_csv(products):
+    yield "upc,name,details,weight,barcode\n"
+    for product in products:
+        yield f"{product.upc},{product.name},{product.details},{product.weight},{product.barcode}\n"
+
+
+@app.get("/product")
+def get_product(
+    response: Response,
+    accept: Annotated[str|None, Header()] = None,
+    x_seed: Annotated[int|None, Header()] = None,
+    quantity: int = 1
+):
+    seed = x_seed or random.randint(0, 100000)
+    rng = random.Random(seed)
+    fake = Faker()
+    fake.seed_instance(seed)
+
+    products = []
+
+    for _ in range(quantity):
+        products.append(
+            Product(
+                upc=fake.ean8(),
+                name=fake.name(),
+                details=fake.text(),
+                weight=fake.random_int(),
+                barcode=fake.ean()
+            )
+        )
+
+
+    match accept:
+        case "text/csv":
+            return StreamingResponse(content=stream_csv(products), media_type="text/csv", headers={"X-Seed": str(seed)})
+        case _:
+            response.headers["X-Seed"] = str(seed)
+            return products
